b1948c1d449fab9e80ca041f9e2534b31873cbfd
Aleksei Slivko <sllexa@mail.ru>
2023-08-09 12:52:35 +0300

    fix: Изменение для работы без файла .env

--- backend/utils/token.js
+++ backend/utils/token.js
@@ -1,20 +1,9 @@
 const jwt = require('jsonwebtoken');
 
-const { NODE_ENV, JWT_SECRET = 'dev-secret' } = process.env;
+const { NODE_ENV, JWT_SECRET } = process.env;
 
-function generateToken(payload) {
-  return jwt.sign(payload, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', { expiresIn: '7d' });
-}
+const generateToken = (payload) => jwt.sign(payload, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', { expiresIn: '7d' });
 
-function checkToken(token) {
-  if (!token) {
-    return false;
-  }
-  try {
-    return jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
-  } catch (err) {
-    return false;
-  }
-}
+const checkToken = (token) => jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
 
 module.exports = { generateToken, checkToken };
